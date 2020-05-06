9e0cd24557c5b392dac5990a8a44f19a635d6d9a
Jaeyeong <201524443@pusan.ac.kr>
2020-05-06 18:01:29 +0900

    상태바 투명하게 처리

--- app/src/main/java/com/example/bookreview/LoginActivity.kt
+++ app/src/main/java/com/example/bookreview/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.bookreview
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.example.bookreview.viewModel.MainViewModel
@@ -22,5 +25,11 @@ class LoginActivity : AppCompatActivity() {
         viewModel.isLoginFinished.observe(this, Observer {
             startActivity(Intent(applicationContext, MainActivity::class.java))
         })
+        this.window.apply {
+            //clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            //addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            statusBarColor = Color.WHITE
+        }
     }
 }
--- app/src/main/java/com/example/bookreview/MainActivity.kt
+++ app/src/main/java/com/example/bookreview/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.bookreview
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import com.example.bookreview.viewModel.MainViewModel
 import com.nhn.android.naverlogin.OAuthLogin
 import com.example.bookreview.viewPager.ViewPagerAdapter
@@ -28,6 +31,14 @@ class MainActivity : AppCompatActivity() {
                 viewpager.setCurrentItem(TabLayout.Tab.INVALID_POSITION, true)
             }
         }.attach()
+
+        //status bar 투명하게 처리
+        this.window.apply {
+            //clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            //addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            statusBarColor = Color.WHITE
+        }
 
         main_search.setOnClickListener {
             val nextIntent = Intent(this, SearchActivity::class.java)
